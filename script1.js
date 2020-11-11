634ce23655db28b2c924a3f830cb78c1210c46e6
anjumunothtrainingsl1 <67541187+anjumunothtrainingsl1@users.noreply.github.com>
2020-11-11 14:04:37 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+function clickImageEventHandler()
+{
+    alert("Thank u for clicking on my image");
+    var imgElement1=document.getElementById("imgFlower1");
+    imgElement1.src="waterfall.jpeg";
+    var imgElement2=document.getElementById("citiLogoImg");
+
+    imgElement2.style.border="15px solid red";
+    
+
+}
